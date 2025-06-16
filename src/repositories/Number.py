808e3a58a79e44bc9a8eddceb26ff9abf4a57951
Gabriel Vieira <nox030705@gmail.com>
2025-06-16 18:25:37 -0400

    refactor: moved `toDecimal` function to Number's method

--- src/repositories/Number.py
+++ src/repositories/Number.py
@@ -47,3 +47,39 @@ class Number:
                 f"Manage-Error: El numero {value} ingresado no es valido")
         self.__isValid = True
         return value
+
+    def toDecimal(self):
+        if self.__value is None:
+            return None
+        value = 0
+        hexDict = {
+            "0": "0",
+            "1": "1",
+            "2": "2",
+            "3": "3",
+            "4": "4",
+            "5": "5",
+            "6": "6",
+            "7": "7",
+            "8": "8",
+            "9": "9",
+            "a": "10",
+            "b": "11",
+            "c": "12",
+            "d": "13",
+            "e": "14",
+            "f": "15"
+        }
+        if "Decimal" in self.__systems:
+            value = int(self.__value)
+        elif "Hexadecimal" in self.__systems and "Binario" not in self.__systems:
+            for i in range(len(str(self.__value))):
+                exponent = len(str(self.__value)) - i - 1
+                n = int(hexDict[str(self.__value)[i].lower()])
+                value += n * 16 ** exponent
+        elif "Binario" in self.__systems:
+            for i in range(len(str(self.__value))):
+                exponent = len(str(self.__value)) - i - 1
+                n = int(str(self.__value)[i])
+                value += n * 2 ** exponent
+        return value
