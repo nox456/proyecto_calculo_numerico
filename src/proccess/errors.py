08a58cc0b2b4a7ec9e0518e022cc4348363ebc9f
Gabriel Vieira <nox030705@gmail.com>
2025-06-17 19:29:33 -0400

    feat: changed errors.log dir and added function name

--- src/proccess/errors.py
+++ src/proccess/errors.py
@@ -8,12 +8,12 @@ from repositories.FileManager import FileManager
 def createLogFile(manager: FileManager, error: Exception, trace: type(Exception.__traceback__), value: Any) -> None:
     tb = traceback.extract_tb(trace)
     oldPath = manager.getPath()
-    manager.setRouter("./src/storage/")
+    manager.setRouter(".")
 
     now = datetime.now().strftime("%d/%m/%Y")
     serial = randint(1000, 9999)
     errorName = error.__class__.__name__
-    ubication = f"{tb[0].filename}: linea {tb[0].lineno}"
+    ubication = f"{tb[0].filename}: linea {tb[0].lineno} / {tb[0].name}"
 
     errorLog = f"{errorName}_{now}_{serial}: [{error} / {value} / {ubication}]\n"
     manager.writeFile("errors.log", errorLog)
